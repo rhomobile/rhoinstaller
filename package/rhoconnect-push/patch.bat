067f44fe3a5f93f08f0cf90b434cae7aaaddfa76
Stanislav Volodarskiy <stan_volodarsky@mail.ru>
2012-10-02 00:35:29 +0400

    remove tgz version from patch.bat

--- package/rhoconnect-push/patch.bat
+++ package/rhoconnect-push/patch.bat
@@ -1,4 +1,4 @@
 set path=%path%
 echo %path%
-npm install -g ./rhoconnect-push/rhoconnect-push-1.0.1.tgz
+for %%i in (rhoconnect-push\rhoconnect-push-*.tgz) do npm install -g %%i
 rhoconnect-push -c ./rhoconnect-push/config.json
